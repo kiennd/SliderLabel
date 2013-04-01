3b12634fac731c7546079acc0802128599779dd2
kiennd <kien.nguyenduc08@gmail.com>
2013-04-01 12:48:46 +0700

    Initial Commit

---
+++ SliderLabel/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SliderLabel
+//
+//  Created by KIENND on 4/1/13.
+//  Copyright (c) 2013 KIENND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SliderLabel/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SliderLabel
+//
+//  Created by KIENND on 4/1/13.
+//  Copyright (c) 2013 KIENND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
